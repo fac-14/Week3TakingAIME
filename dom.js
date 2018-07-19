664071910caa339775562e4008f34df9014c9e82
Kate Beard <asouthernbellein@gmail.com>
2018-07-19 12:44:02 +0100

    SVG! And learned git stash git add apply

--- dom.js
+++ dom.js
@@ -1,8 +1,14 @@
+// event listener for submit button
+
 document
+  // get the value from the drop-down form
   .getElementById("line-select")
+  // add event listener to submit button
   .addEventListener("submit", function(event) {
+    // prevent refresh
     event.preventDefault();
     var line = document.getElementById("tube-lines");
     var value = line.options[line.selectedIndex].value;
+    // calls getLineStatus function
     apiFunctions.getLineStatus(value);
   });
